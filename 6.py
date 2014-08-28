8845632c686e4e304abce133baa760eda2f875a3
fguan <frankgimpy@maozer.com>
2014-08-28 02:25:54 -0400

    Add problem description

--- 6.py
+++ 6.py
@@ -12,4 +12,5 @@ def squareOfSums(limit):
         sum += i
     return sum * sum
 
+"""Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum."""
 print(squareOfSums(100) - sumOfSquares(100))
