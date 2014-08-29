cb9ce9d0efc08d83afd9cf1734fe603970229c57
fguan <frankgimpy@maozer.com>
2014-08-29 18:34:06 -0400

    slight optimization

--- 20.py
+++ 20.py
@@ -10,10 +10,8 @@ product = 1
 for i in range(1, 101):
     product *= i
 
-length = len(str(product))
-
 sum = 0
-for j in range(0, length):
-    sum += int(str(product)[j])
+for j in str(product):
+    sum += int(j)
 
 print(sum)
